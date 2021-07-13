bdee9d3a5dd46f42684a1586fb27ee924ae5f210
David Figueroa <figueroada@vcu.edu>
2021-07-13 03:06:19 -0400

    fixed nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import '../../index.css';
-// import logo from '../../images/logo.png';
+import logo from '../../images/logo.png';
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
 
 function Nav() {
     return(
-    // <div className="logo"> <img src={logo} alt="logo"> </div>
-    // <button className="nav-toggle" aria-label="toggle navigation">
-    // <span className="hamburger"></span>
-    // </button>
+    <>
+        <div className="logo"> <img src={logo} alt="logo"/> </div>
 
-    <nav className="nav">
-    <ul className="nav__list">
-        <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-        {/* <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li> */}
-        <li className="nav__item"><a href="#about" className="nav__link">About me</a></li>
-        <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
-        <li className="nav__item"><a href="#contactme" className="nav__link">Contact me</a></li>
-    </ul>
-    </nav>
+        <button className="nav-toggle" aria-label="toggle navigation">
+            <span className="hamburger"></span>
+        </button>
+    
+        <nav className="nav">
+            <ul className="nav__list">
+                <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
+                {/* <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li> */}
+                <li className="nav__item"><a href="#about" className="nav__link">About me</a></li>
+                <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
+                <li className="nav__item"><a href="#contactme" className="nav__link">Contact me</a></li>
+            </ul>
+        </nav>
+    </>
     );
 }
 
 export default Nav;
+
